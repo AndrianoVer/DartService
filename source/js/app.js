da8108aa25829044f7c2863be6e558a9dd6bb07d
Andrew Verbitsky <averbitskiy5@gmail.com>
2020-04-24 17:07:36 +0300

    media queries

--- source/js/app.js
+++ source/js/app.js
@@ -10,3 +10,14 @@ btnFree.onmouseleave = () => {
 	btnBuy.style.background = '#061e37';
 	btnBuy.style.color = '#fff';
 }
+
+let headerBurger = document.querySelector('.header__burger');
+let siteNavigation = document.querySelector('.site__navigation')
+let body = document.querySelector('body');
+
+headerBurger.onclick = function (event) {
+	headerBurger.classList.toggle('active');
+	siteNavigation.classList.toggle('active');
+	body.classList.toggle('lock');
+
+}
