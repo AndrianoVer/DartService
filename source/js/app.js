aef38a323a1b05c798c8cd62886f9361de12eff4
Andrew Verbitsky <averbitskiy5@gmail.com>
2020-05-02 11:53:41 +0300

    add new font+ new section

--- source/js/app.js
+++ source/js/app.js
@@ -21,3 +21,17 @@ headerBurger.onclick = function (event) {
 	body.classList.toggle('lock');
 
 }
+
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+	if (!e.target.matches('.dropbtn')) {
+		var myDropdown = document.getElementById("myDropdown");
+		if (myDropdown.classList.contains('show')) {
+			myDropdown.classList.remove('show');
+		}
+	}
+}
