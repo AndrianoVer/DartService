811281aed4b2e65720f411d3abab52db4738c395
Andrew Verbitsky <averbitskiy5@gmail.com>
2020-05-04 21:06:26 +0300

    commit

--- source/js/app.js
+++ source/js/app.js
@@ -22,16 +22,14 @@ headerBurger.onclick = function (event) {
 
 }
 
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-}
+window.onload = function () {
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (e) {
-	if (!e.target.matches('.dropbtn')) {
-		var myDropdown = document.getElementById("myDropdown");
-		if (myDropdown.classList.contains('show')) {
-			myDropdown.classList.remove('show');
+	document.querySelector('.dropdown-menu').addEventListener('click', dropDown);
+
+	function dropDown(event) {
+		if (event.target.className == 'dropdowm-menu-item-header') {
+			console.log(event.target.getAttribute('data-tab'));
 		}
 	}
+
 }
